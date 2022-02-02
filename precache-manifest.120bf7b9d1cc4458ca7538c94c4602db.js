64f3a07269f4b0461abcc44e84bc52d9b7e9661b
Bishal-sys <96173584+BishalDas-sys@users.noreply.github.com>
2022-02-02 15:46:40 +0530

    Updates

---
+++ precache-manifest.120bf7b9d1cc4458ca7538c94c4602db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cf8de0b4e45564953efebf4ca140d6",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "a7ebb137a80a757a16c5",
+    "url": "/TodoList/static/css/main.f5b74edd.chunk.css"
+  },
+  {
+    "revision": "5c3c1742a40c4bf5a9fa",
+    "url": "/TodoList/static/js/2.84e277a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.84e277a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ebb137a80a757a16c5",
+    "url": "/TodoList/static/js/main.07d48e90.chunk.js"
+  },
+  {
+    "revision": "ab319c1b809d151f70fd",
+    "url": "/TodoList/static/js/runtime-main.74e8c3cf.js"
+  }
+]);
